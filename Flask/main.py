573ea0e4feea9b2f062984d03a918200b9a0d22a
Perry Spector <pspector@bgc.org>
2020-06-05 09:11:56 -0700

    refactored

--- Flask/main.py
+++ Flask/main.py
@@ -1,29 +1,32 @@
+import numpy as np
+import pandas as pd
+import sqlalchemy
+import copy
+
+# App packages
 import flask
 from flask import Flask, render_template, request, send_file
 from flask import Response
-
-from PIL import Image
 from werkzeug.utils import secure_filename
 
-import numpy as np
-import base64
-from io import BytesIO
-from matplotlib.figure import Figure
-import copy
-
+# Model-related packages
 import sys
 sys.path.insert(0, "../scripts")
-from image_processing import *
-import sqlalchemy
-import pandas as pd
-
 import keras_ocr
-
 import torch
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.transforms import functional as F
 
+# Packages required for image processing and figure generation
+from PIL import Image
+from matplotlib.figure import Figure
+from image_processing import *
+import base64
+from io import BytesIO
+
+#-----------------------------------------------
+#                 functions
 #-----------------------------------------------
 
 def generate_plot(img, bboxes, labels):
@@ -97,11 +100,31 @@ def get_cereal(df, ocr_words):
 
     return df.sort_values(by=['jaccard'], ascending=False)['cereal_name'].iloc[0]
 
+def predict(img, df):
+    # convert to PyTorch tensor and unsqueeze
+    preprocessed_img = F.to_tensor(img).unsqueeze(0)
+    # predict
+    with torch.no_grad():                                     # Without tracking gradients,
+        pred = model(preprocessed_img)
+    bboxes = np.asarray(pred[0]['boxes'])
+    #labels = np.asarray(pred[0]['labels'])
 
+    # OCR
+    labels = []
+    for box in bboxes:
+        sub_img = copy.deepcopy(img).crop((box))
+        predictions = pipeline.recognize([np.asarray(sub_img)])[0]
+        ocr_words = set([pred[0] for pred in predictions]) # make set for jaccard sim
+        df2 = copy.deepcopy(df)
+        labels.append(get_cereal(df2, ocr_words))
+
+    return bboxes, labels
+
+#-----------------------------------------------
+#  load models and query DB during app spinup
 #-----------------------------------------------
 
 # load RCNN model
-#num_classes=len(df['label_id'])
 num_classes = 2
 model = load_RCNN(num_classes)
 
@@ -112,6 +135,9 @@ pipeline = keras_ocr.pipeline.Pipeline()
 s = 'select cereal_name, company, short_name, label_id from cereals'
 df = pd.read_sql(s, connect_to_db())
 
+
+#-----------------------------------------------
+#                run app
 #-----------------------------------------------
 
 app = Flask(__name__)
@@ -126,45 +152,9 @@ def make_prediction():
         file = request.files['image']
         filename = secure_filename(file.filename)
         img = Image.open(file).convert("RGB")
-        print(filename)
-        print(type(img))
 
-        # set output image size
-        #out_px = 224
-        # resize and crop
-        #img = resize_bg_img(img, out_px)
-
-        # convert to PyTorch tensor and unsqueeze
-        preprocessed_img = F.to_tensor(img).unsqueeze(0)
-
-        with torch.no_grad():                                     # Without tracking gradients,
-            pred = model(preprocessed_img)
-
-        bboxes = np.asarray(pred[0]['boxes'])
-        labels = np.asarray(pred[0]['labels'])
-
-        print(len(bboxes))
-
-        #-------------------------------------------------
-
-        labels = []
-        for box in bboxes:
-            sub_img = copy.deepcopy(img).crop((box))
-            predictions = pipeline.recognize([np.asarray(sub_img)])[0]
-
-            ocr_words = set([pred[0] for pred in predictions]) # make set for jaccard sim
-
-            df2 = copy.deepcopy(df)
-
-            labels.append(get_cereal(df2, ocr_words))
-
-
-
-
-
-
-
-        #-------------------------------------------------
+        # process image, predict bounding boxes, predict cereal using OCR
+        bboxes, labels = predict(img, df)
 
         # generate figure with matplotlib
         figure = generate_plot(img, bboxes, labels)
